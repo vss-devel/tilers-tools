db495d5a8cfcac5319845b6016f776613a1e0d0f
vadp <vadp@localhost>
2011-02-09 17:04:38 +0300

    tilers-tools.bat: a touch

--- tilers_tools/tilers-tools.bat
+++ tilers_tools/tilers-tools.bat
@@ -7,7 +7,7 @@ goto exit
 
 :setenv
 set PYTHON=D:\bin\Python26
-set GDAL=D:\bin\gdal-mapserver
+set GDAL=D:\bin\release-1600-gdal-1-8-0-mapserver-5-6-6
 
 set TILERS_TOOLS=%~dp0
 rem Remove trailing backslash
