e8c34fa19c6bd36f13ecb58d420ac09708a05d9c
vadp <vadp@localhost>
2011-05-15 18:00:37 +0400

    poi2mapper.py: cosmetic changes in strip_html

--- tilers_tools/poi2mapper.py
+++ tilers_tools/poi2mapper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# 2011-05-11 17:55:10 
+# 2011-05-15 17:27:24 
 
 ###############################################################################
 # Copyright (c) 2010, Vadim Shlyakhov
@@ -41,39 +41,28 @@ def re_subs(sub_list,l):
         l=re.sub(pattern,repl,l)
     return l
 
-##
-# Removes HTML markup from a text string.
-# http://effbot.org/zone/re-sub.htm#strip-html
-#
-# @param text The HTML source.
-# @return The plain text.  If the HTML source contains non-ASCII
-#     entities or character references, this is a Unicode string.
-
 def strip_html(text):
-    def fixup(m):
-        text = m.group(0)
-        if text[:1] == "<":
+    'Removes HTML markup from a text string. http://effbot.org/zone/re-sub.htm#strip-html'
+    
+    def replace(match): # pattern replacement function
+        text = match.group(0)
+        if text[0] == "<":
             return "" # ignore tags
-        if text[:2] == "&#":
-            try:
-                if text[:3] == "&#x":
-                    return unichr(int(text[3:-1], 16))
-                else:
-                    return unichr(int(text[2:-1]))
-            except ValueError:
-                pass
-        elif text[:1] == "&":
-            entity = htmlentitydefs.entitydefs.get(text[1:-1])
-            if entity:
-                if entity[:2] == "&#":
-                    try:
-                        return unichr(int(entity[2:-1]))
-                    except ValueError:
-                        pass
-                else:
-                    return unicode(entity, "iso-8859-1")
+        if text[0] == "&":
+            if text[1] == "#":
+                try:
+                    if text[2] == "x":
+                        return unichr(int(text[3:-1], 16))
+                    else:
+                        return unichr(int(text[2:-1]))
+                except ValueError:
+                    pass
+            else:
+                return unichr(htmlentitydefs.name2codepoint[text[1:-1]])
         return text # leave as is
-    return re.sub("(?s)<[^>]*>|&#?\w+;", fixup, text)
+        # fixup end
+
+    return re.sub("(?s)<[^>]*>|&#?\w+;", replace, text)
 
 def attr_update(self,**updates):
         self.__dict__.update(updates)
