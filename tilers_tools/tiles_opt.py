05139a68dea5dfc59aee53b3dee071f7bd620871
vadp <vadp@localhost>
2011-05-15 14:55:13 +0400

    tiles_opt.py: minor touches on JPEG code

--- tilers_tools/tiles_opt.py
+++ tilers_tools/tiles_opt.py
@@ -38,12 +38,24 @@ def find_by_ext(path, ext):
     return flatten([os.path.join(path, name) for name in files if name.endswith(ext)] 
                     for path, dirs, files in os.walk(src_dir))
 
+tick_rate=50
+tick_count=0
+
+def counter():
+    global tick_count
+    tick_count+=1
+    if tick_count % tick_rate == 0:
+        pf('.',end='')
+        return True
+    else:
+        return False
+
 class KeyboardInterruptError(Exception): pass
 
 def optimize_png(fl):
     'optimize png using pngnq utility'
+    counter()
     try:
-        pf('.',end='')
         command(['pngnq','-fn', options.colors, fl])
     except KeyboardInterrupt: # http://jessenoller.com/2009/01/08/multiprocessingpool-and-keyboardinterrupt/
         pf('got KeyboardInterrupt')
@@ -51,9 +63,14 @@ def optimize_png(fl):
 
 def to_jpeg(fl):
     'convert to jpeg'
-    dst_fl=os.path.splitext(fl)[0]+'.jpg'
-    img=Image.open(fl)
-    img.save(dst_fl,optimize=True,quality=options.quality)
+    counter()
+    try:
+        dst_fl=os.path.splitext(fl)[0]+'.jpg'
+        img=Image.open(fl)
+        img.save(dst_fl,optimize=True,quality=options.quality)
+    except KeyboardInterrupt: # http://jessenoller.com/2009/01/08/multiprocessingpool-and-keyboardinterrupt/
+        pf('got KeyboardInterrupt')
+        raise KeyboardInterruptError()
    
 if __name__=='__main__':
     logging.basicConfig(level=logging.INFO)
@@ -75,19 +92,20 @@ if __name__=='__main__':
         parser.error('No input directory(s) specified')
 
     for src_dir in args:
-        print src_dir
+        pf(src_dir,end='')
         # delete rouge *-nq8.png if any
         map(os.remove, find_by_ext(src_dir, '-nq8.png'))
         # find *.png
         src_lst=filter(lambda fl: not stat.S_ISLNK(os.lstat(fl)[stat.ST_MODE]), # skip symlinks
                     find_by_ext(src_dir, '.png'))
 
-        if options.jpeg:
-            re_sub_file(os.path.join(src_dir,'gmaps.html'),
-                    [('tile_ext = .*;','tile_ext = ".jpg";')])
-            parallel_map(to_jpeg,src_lst)
-            
+        if options.jpeg:                
+            parallel_map(to_jpeg,src_lst)            
             map(os.remove,src_lst)
+            
+            gmaps=os.path.join(src_dir,'gmaps.html')
+            if os.path.exists(gmaps):
+                re_sub_file(gmaps,[('tile_ext = .*;','tile_ext = ".jpg";')])
         else:        
             parallel_map(optimize_png,src_lst)
 
