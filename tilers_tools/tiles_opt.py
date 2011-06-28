ce38a877b44f62894f22daa70a1676f8e2e44018
vadp <vadp@localhost>
2011-06-28 14:49:44 +0400

    tiles_opt.py: create result in a separate directory

--- tilers_tools/tiles_opt.py
+++ tilers_tools/tiles_opt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# 2011-05-11 11:09:08 
+# 2011-06-28 12:24:50 
 
 ###############################################################################
 # Copyright (c) 2010, Vadim Shlyakhov
@@ -34,10 +34,6 @@ from PIL import Image
 
 from tiler_functions import *
 
-def find_by_ext(path, ext):
-    return flatten([os.path.join(path, name) for name in files if name.endswith(ext)] 
-                    for path, dirs, files in os.walk(src_dir))
-
 tick_rate=50
 tick_count=0
 
@@ -50,67 +46,86 @@ def counter():
     else:
         return False
 
-class KeyboardInterruptError(Exception): pass
-
-def optimize_png(fl):
+def optimize_png(src,dst,dpath):
     'optimize png using pngnq utility'
-    counter()
-    try:
-        command(['pngnq','-fn', options.colors, fl])
-    except KeyboardInterrupt: # http://jessenoller.com/2009/01/08/multiprocessingpool-and-keyboardinterrupt/
-        pf('got KeyboardInterrupt')
-        raise KeyboardInterruptError()
+    command(['pngnq','-n',options.colors,'-e','.png','-d',dpath,src])
 
-def to_jpeg(fl):
+def to_jpeg(src,dst,dpath):
     'convert to jpeg'
-    counter()
+    dst_jpg=os.path.splitext(dst)[0]+'.jpg'
+    img=Image.open(src)
+    img.save(dst_jpg,optimize=True,quality=options.quality)
+
+class KeyboardInterruptError(Exception): pass
+   
+def proc_file(f):
     try:
-        dst_fl=os.path.splitext(fl)[0]+'.jpg'
-        img=Image.open(fl)
-        img.save(dst_fl,optimize=True,quality=options.quality)
+        src=os.path.join(src_dir,f)
+        dst=os.path.join(dst_dir,f)
+        dpath=os.path.split(dst)[0]
+        if not os.path.exists(dpath):
+            os.makedirs(dpath)
+        if f.lower().endswith('.png'):
+            optimize_png(src,dst,dpath)
+        else:
+            shutil.copy(src,dpath)
+        counter()
     except KeyboardInterrupt: # http://jessenoller.com/2009/01/08/multiprocessingpool-and-keyboardinterrupt/
         pf('got KeyboardInterrupt')
         raise KeyboardInterruptError()
-   
+    
 if __name__=='__main__':
     logging.basicConfig(level=logging.INFO)
     #logging.basicConfig(level=logging.DEBUG)
 
     parser = optparse.OptionParser()
     usage = "usage: %prog [options] arg"
-    parser.add_option("-q", "--quiet", action="store_true")
     parser.add_option("-n", "--colors", dest="colors", default='256',
         help='Specifies  the  number  of colors to quantize to (default: 256)')
     parser.add_option("--jpeg", action="store_true",
         help='convert tiles to JPEG')
     parser.add_option("--quality", dest="quality", type="int", default=75,
         help='JPEG quality (default: 75)')
+    parser.add_option("-r", "--remove-dest", action="store_true",
+        help='delete destination directory if any')
+    parser.add_option("-q", "--quiet", action="store_true")
+    parser.add_option("-d", "--debug", action="store_true")
+    parser.add_option("--nothreads", action="store_true",
+        help="do not use multiprocessing")
         
     (options, args) = parser.parse_args()
 
     if not args:
         parser.error('No input directory(s) specified')
 
-    for src_dir in args:
-        pf(src_dir,end='')
-        # delete rouge *-nq8.png if any
-        map(os.remove, find_by_ext(src_dir, '-nq8.png'))
-        # find *.png
-        src_lst=filter(lambda fl: not stat.S_ISLNK(os.lstat(fl)[stat.ST_MODE]), # skip symlinks
-                    find_by_ext(src_dir, '.png'))
+    if options.nothreads or options.debug:
+        set_nothreads()
 
-        if options.jpeg:                
-            parallel_map(to_jpeg,src_lst)            
-            map(os.remove,src_lst)
+    if options.jpeg: 
+        optimize_png=to_jpeg
+
+    for src_dir in args:
+        dst_dir=src_dir+'.opt'
+        pf('%s -> %s ' % (src_dir,dst_dir),end='')
+
+        if options.remove_dest: 
+            shutil.rmtree(dst_dir,ignore_errors=True)
+
+        # find all source files
+        try:
+            cwd=os.getcwd()
+            os.chdir(src_dir)
+            src_lst=flatten([os.path.join(path, name) for name in files] 
+                        for path, dirs, files in os.walk('.'))
+        finally:
+            os.chdir(cwd)
+
+        parallel_map(proc_file,src_lst)            
             
-            gmaps=os.path.join(src_dir,'gmaps.html')
+        if options.jpeg: 
+            gmaps=os.path.join(dst_dir,'gmaps.html')
             if os.path.exists(gmaps):
                 re_sub_file(gmaps,[('tile_ext = .*;','tile_ext = ".jpg";')])
-        else:        
-            parallel_map(optimize_png,src_lst)
-
-            # 'pngnq' creates *-nq8.png files, so rename files back to original names
-            map(lambda f: os.rename(f,f[:-len('-nq8.png')]+'.png'), find_by_ext(src_dir, '-nq8.png'))
 
         pf('')
 
