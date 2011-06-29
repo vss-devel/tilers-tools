1e0a19118cd9714b4fd286bdd5ae4e204190cfd8
vadp <vadp@localhost>
2011-06-29 13:33:24 +0400

    tiles_merge.py: "underlay" and "modify_htmls" rearranged + tile parameter options

--- tilers_tools/tiles_merge.py
+++ tilers_tools/tiles_merge.py
@@ -40,39 +40,40 @@ from tiler_functions import *
 class KeyboardInterruptError(Exception): 
     pass
 
-def modify_htmls(src_dir, dest_dir):
+def modify_htmls(src_dir, dst_dir):
     'adjusts destination gmaps.html and returns tile style (gmaps,TMS)'
     googlemaps='gmaps.html'
-    #openlayers='openlayers.html'
 
-    if not os.path.exists(os.path.join(dest_dir,googlemaps)):
-        try:
-            shutil.copy(os.path.join(src_dir,googlemaps),dest_dir)
-            #shutil.copy(os.path.join(src_dir,openlayers),dest_dir)
-        except: pass
+    s_html,d_html=[os.path.join(d,googlemaps) for d in (src_dir,dst_dir)]
+
+    if not os.path.exists(s_html):
+        return False
+
+    # check if it's TMS type
+    tms_tiles= 'true' in [ i for i in open(s_html) if 'var tms_tiles' in i][0]
+    
+    if not os.path.exists(d_html):
+        shutil.copy(s_html,dst_dir)
     else:
         # get a list of zoom levels
         try:
             cwd=os.getcwd()
-            os.chdir(dest_dir)
+            os.chdir(dst_dir)
             dzooms=sorted([eval(i) for i in glob.glob('[0-9]*')])
         finally:
             os.chdir(cwd)
         zoom_min=dzooms[0]
         zoom_max=dzooms[-1]
 
-        # get src bounds
-        s=[ i for i in open(os.path.join(src_dir,googlemaps))
-            if 'var mapBounds = new G.LatLngBounds' in i][0]
-        num_str=re.sub('[^-,.0-9]*','',re.sub('\.Lat*','',s)) # leave only numbers there
-        s_bounds=map(float,num_str.split(','))
-
-        # get dest bounds
-        s=[ i for i in open(os.path.join(dest_dir,googlemaps))
-            if 'var mapBounds = new G.LatLngBounds' in i][0]
-        num_str=re.sub('[^-,.0-9]*','',re.sub('\.Lat*','',s)) # leave only numbers there
-        d_bounds=map(float,num_str.split(','))
+        bounds=[]
+        for f in (s_html,d_html):
+            txt=[ i for i in open(f)
+                if 'var mapBounds = new G.LatLngBounds' in i][0]
+            num_str=re.sub('[^-,.0-9]*','',re.sub('\.Lat*','',txt)) # leave only numbers there
+            bounds.append(map(float,num_str.split(',')))
+        s_bounds,d_bounds=bounds
         ld((s_bounds,d_bounds))
+
         if s_bounds[0] < d_bounds[0]: d_bounds[0]=s_bounds[0]
         if s_bounds[1] < d_bounds[1]: d_bounds[1]=s_bounds[1]
         if s_bounds[2] > d_bounds[2]: d_bounds[2]=s_bounds[2]
@@ -80,21 +81,16 @@ def modify_htmls(src_dir, dest_dir):
         ld(d_bounds)
 
         # write back modified googlemaps.html
-        chart_name=os.path.split(dest_dir)[1]
+        map_name=os.path.split(dst_dir)[1]
         subs=[("(var mapBounds = new G.LatLngBounds).*;",
                 "\\1( new G.LatLng(%f, %f), new G.LatLng(%f, %f));" % tuple(d_bounds)),
             ('(var mapMinZoom =).*;','\\1 %i;' % zoom_min),
             ('(var mapMaxZoom =).*;','\\1 %i;' % zoom_max),
-            ('<title>.*</title>','<title>%s</title>' % chart_name),
-            ('<h1>.*</h1>','<h1>%s</h1>' % chart_name)]
-        re_sub_file(os.path.join(dest_dir,googlemaps), subs)
+            ('<title>.*</title>','<title>%s</title>' % map_name),
+            ('<h1>.*</h1>','<h1>%s</h1>' % map_name)]
+        re_sub_file(d_html, subs)
 
-        # check if it's TMS type
-        s=[ i for i in open(os.path.join(src_dir,googlemaps))
-            if 'var tms_tiles' in i][0]
-        tms_tiles= 'true' in s
-
-        return tms_tiles
+    return tms_tiles
 
 def transparency(img):
     'estimate transparency of an image'
@@ -103,8 +99,10 @@ def transparency(img):
     return 1 if a_min == 255 else 0 if a_max == 0 else -1
 
 class MergeSet:
-    def __init__(self,src_dir,dest_dir):
-        (self.src,self.dest)=(src_dir,dest_dir)
+    def __init__(self,src_dir,dst_dir):
+        (self.src,self.dest)=(src_dir,dst_dir)
+        self.tile_sz=tuple(map(int,options.tile_size.split(',')))
+
         if options.strip_src_ext:
             self.src=os.path.splitext(src)[0]
         if options.add_src_ext is not None:
@@ -128,6 +126,13 @@ class MergeSet:
             ld("cache load failed")
         ld(repr(self.src_transp))
         
+        # define crop map for underlay function
+        tsx,tsy=self.tile_sz
+        self.underlay_map=[          #    lf    up    rt    lw
+            (   0,    0,tsx/2,tsy/2), (tsx/2,    0,  tsx,tsy/2),
+            (   0,tsy/2,tsx/2,  tsy), (tsx/2,tsy/2,  tsx,  tsy),
+            ]
+
         # do the thing
         self.merge_dirs()
 
@@ -141,45 +146,37 @@ class MergeSet:
         (z,y,x)=map(int,(z,y,x))
         if z < self.max_zoom:
             return
-        if self.tms_tiles:
-            tiles_map=[
-                (  0,128,128,256), (128,128,256,256),
-                (  0,  0,128,128), (128,  0,256,128),
-                ]
-        else:
-            tiles_map=[
-                (  0,  0,128,128), (128,  0,256,128),
-                (  0,128,128,256), (128,128,256,256),
-                ]
-        tiles_in=[(x*2,y*2), (x*2+1,y*2),
-                  (x*2,y*2+1),(x*2+1,y*2+1)]
-        for (src_xy,out_loc) in zip(tiles_in,tiles_map):
-            dest_tile='%i/%i/%i%s' % (z+1,src_xy[0],src_xy[1],ext)
-            dest_path=os.path.join(self.dest,dest_tile)
-            if not os.path.exists(dest_path):
+
+        dz,dx,dy=z+1,x*2,y*2
+        dst_tiles=[(dx,dy),  (dx+1,dy),
+                   (dx,dy+1),(dx+1,dy+1)]
+        for (dst_xy,src_area) in zip(dst_tiles,self.underlay_map):
+            dst_tile='%i/%i/%i%s' % (dz,dst_xy[0],dst_xy[1],ext)
+            dst_path=os.path.join(self.dest,dst_tile)
+            if not os.path.exists(dst_path):
                 continue
-            dest_raster=Image.open(dest_path).convert("RGBA")
-            if transparency(dest_raster) == 1: # lower tile is fully opaque
+            dst_raster=Image.open(dst_path).convert("RGBA")
+            if transparency(dst_raster) == 1: # lower tile is fully opaque
                 continue
             if not src_raster: # check if opening was deferred
                 src_raster=Image.open(src_path).convert("RGBA")
-            out_raster=src_raster.crop(out_loc).resize((256,256),Image.BILINEAR)
-            out_raster=Image.composite(dest_raster,out_raster,dest_raster)
-            del dest_raster
-            out_raster.save(dest_path)
+            out_raster=src_raster.crop(src_area).resize(self.tile_sz,Image.BILINEAR)
+            out_raster=Image.composite(dst_raster,out_raster,dst_raster)
+            del dst_raster
+            out_raster.save(dst_path)
 
             if options.debug:
                 pf('%i'%level,end='')
             else:
                 pf('#',end='')            
-            self.underlay(dest_tile,dest_path,out_raster,level)
+            self.underlay(dst_tile,dst_path,out_raster,level)
                 
     def __call__(self,tile):
         '''called by map() to merge a source tile into the destination tile set'''
         try:
             src_path=os.path.join(self.src,tile)
-            dest_tile=os.path.join(self.dest,tile)
-            dpath=os.path.dirname(dest_tile)
+            dst_tile=os.path.join(self.dest,tile)
+            dpath=os.path.dirname(dst_tile)
             if not os.path.exists(dpath):
                 try: # thread race safety
                     os.makedirs(dpath)
@@ -194,16 +191,16 @@ class MergeSet:
                 #pf('-',end='')
                 #os.remove(src_path)
                 pass
-            elif transp == 1 or not os.path.exists(dest_tile): 
+            elif transp == 1 or not os.path.exists(dst_tile): 
                 # fully opaque or no destination tile exists yet
                 #pf('>',end='')
-                shutil.copy(src_path,dest_tile)
+                shutil.copy(src_path,dst_tile)
             else: # semitransparent, combine with destination (exists! see above)
                 pf('+',end='')
                 if not src_raster: 
                     src_raster=Image.open(src_path).convert("RGBA")
-                dst_raster=Image.composite(src_raster,Image.open(dest_tile),src_raster)
-                dst_raster.save(dest_tile)
+                dst_raster=Image.composite(src_raster,Image.open(dst_tile),src_raster)
+                dst_raster.save(dst_tile)
             if options.underlay and transp != 0:
                 self.underlay(tile,src_path,src_raster,options.underlay)
         except KeyboardInterrupt: # http://jessenoller.com/2009/01/08/multiprocessingpool-and-keyboardinterrupt/
@@ -211,8 +208,8 @@ class MergeSet:
             raise KeyboardInterruptError()
         return (tile,transp) # send back transparency values for caching
 
-    def upd_stat(self,stat):
-        self.src_transp.update(dict(stat))
+    def upd_stat(self,transparency_data):
+        self.src_transp.update(dict(transparency_data))
         try:
             pickle.dump(self.src_transp,open(self.src_cache_path,'w'))
         except:
@@ -220,27 +217,36 @@ class MergeSet:
         pf('')
 
     def merge_dirs(self):
-        self.tms_tiles=modify_htmls(self.src, self.dest)
-        res=parallel_map(self,self.src_lst)
-        self.upd_stat(res)
+        tms_html=modify_htmls(self.src, self.dest)
+
+        if options.tms or tms_html: # rearrange underlay crop map for TMS tiles
+            m=self.underlay_map
+            self.underlay_map=[m[2],m[3],m[0],m[1]]
+
+        src_transparency=parallel_map(self,self.src_lst)
+        self.upd_stat(src_transparency)
 
 # MergeSet end
 
 if __name__=='__main__':
     parser = optparse.OptionParser(
-        usage="usage: %prog [--cut] [--dest-dir=DEST_DIR] <tile_dirs>... <target_dir>",
+        usage="usage: %prog [--cut] [--dest-dir=DST_DIR] <tile_dirs>... <target_dir>",
         version=version,
         description="")
     parser.add_option("-r", "--remove-dest", action="store_true",
         help='delete destination directory before merging')
     parser.add_option("-l", "--src-list", default=None,
-        help='read source directories list from a file; if no destination is provided then name destination after list file without a suffix')
+        help='read a list of source directories from a file; if no destination is provided then name destination after the list file without a suffix')
     parser.add_option("-s", "--strip-src-ext", action="store_true",
         help='strip extension suffix from a source parameter')
     parser.add_option("-x", "--add-src-ext", default=None,
         help='add extension suffix to a source parameter')
     parser.add_option('-u',"--underlay", type='int', default=0,
         help="underlay semitransparent tiles with a zoomed-in raster from a higher level")
+    parser.add_option("--tms", action="store_true",
+        help="force TMS type tiles")
+    parser.add_option("--tile-size", default='256,256',metavar="SIZE_X,SIZE_Y",
+        help='tile size (default: 256,256)')
     parser.add_option("-q", "--quiet", action="store_true")
     parser.add_option("-d", "--debug", action="store_true")
     parser.add_option("--nothreads", action="store_true",
@@ -255,13 +261,13 @@ if __name__=='__main__':
     if options.src_list:
         src_dirs=[i.rstrip('\n') for i in open(options.src_list,'r')]
         try:
-            dest_dir=args[-1]
+            dst_dir=args[-1]
         except:
-            dest_dir=os.path.splitext(options.src_list)[0]
+            dst_dir=os.path.splitext(options.src_list)[0]
     else:
         try:
             src_dirs=args[0:-1]
-            dest_dir=args[-1]
+            dst_dir=args[-1]
         except:
             raise Exception("No source(s) or/and destination specified")
 
@@ -269,14 +275,14 @@ if __name__=='__main__':
         set_nothreads()
 
     if options.remove_dest: 
-        shutil.rmtree(dest_dir,ignore_errors=True)
+        shutil.rmtree(dst_dir,ignore_errors=True)
         
-    if not os.path.exists(dest_dir):
+    if not os.path.exists(dst_dir):
         try:
-            os.makedirs(dest_dir)
+            os.makedirs(dst_dir)
         except os.error: pass
 
     for src in src_dirs:
         if not (src.startswith("#") or src.strip() == ''): # ignore sources with names starting with "#" 
-            MergeSet(src, dest_dir)
+            MergeSet(src, dst_dir)
 
